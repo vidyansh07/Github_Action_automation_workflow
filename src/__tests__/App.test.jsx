3566321e17455ddd2747d9a3e788fe701a947128
vidyansh07 <vidyansht@gmail.com>
2025-02-22 15:29:44 +0530

    testing the deployed frontend

--- src/__tests__/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { fireEvent, render, screen } from '@testing-library/react'
-import App from '../App'
-
-test('renders vite link', () => {
-  render(<App />)
-  const headerElement = screen.getByText(/Vite \+ React/i);
-  expect(headerElement).toBeInTheDocument();
-});
